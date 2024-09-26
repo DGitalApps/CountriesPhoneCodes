b141755014fff0c6fe518ca4d7ecf108e03233ac
DAMIAN-PC\DAMIAN <contactmobiledevelop@gmail.com>
2024-09-26 23:03:21 +0200

    fixes

--- CountriesPhoneCode/build.gradle.kts
+++ CountriesPhoneCode/build.gradle.kts
@@ -25,16 +25,19 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
 
 dependencies {
     implementation(libs.androidx.annotation.jvm)
+    implementation(libs.androidx.monitor)
+    implementation(libs.androidx.junit.ktx)
+    testImplementation(libs.testng)
 }
 
 afterEvaluate {
